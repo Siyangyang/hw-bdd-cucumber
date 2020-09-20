d5a6006adc0c00f19fd7141fba4c35ecf0f7a7dd
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2020-09-20 04:46:52 +0000

    part 2 finished

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,11 @@
 # Add a declarative step here for populating the DB with movies.
 
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
     Movie.create(movie)
-    # puts movie
   end
   # fail "Unimplemented"
 end
@@ -31,10 +31,21 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #puts rating_list
+  new_rating_list = rating_list.split(", ")
+  new_rating_list.each do |rating|
+    if uncheck == nil
+      check("ratings[#{rating}]")
+    else
+      uncheck("ratings[#{rating}]")
+    end
+  end
+  
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(Movie.count).to eq 10
+
+  #page.should have_selector('tbody/tr', count: Movie.count) 
 end
